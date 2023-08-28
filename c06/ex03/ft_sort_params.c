3ae46bf329f1c48eb6b6214a03c93a0c6e224e53
Brahim Jandri <125567781+BrahimJandri@users.noreply.github.com>
2023-08-28 13:27:18 +0100

    Add files via upload

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjandri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 13:38:24 by bjandri           #+#    #+#             */
+/*   Updated: 2023/08/28 10:53:26 by bjandri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int ac, char **av)
+{
+	char	*temp;
+	int	i;
+
+
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		write(1, &str[i], 1);
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+
+}
