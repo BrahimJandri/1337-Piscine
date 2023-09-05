7f3c6ebe116f5a97b0d9e566ca304072785aa198
Brahim Jandri <125567781+BrahimJandri@users.noreply.github.com>
2023-09-05 22:36:18 +0100

    Add files via upload

---
+++ c08/c08/c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjandri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 09:29:04 by bjandri           #+#    #+#             */
+/*   Updated: 2023/09/04 21:09:22 by bjandri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
