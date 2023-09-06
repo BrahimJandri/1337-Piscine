b5f10af034783aa13c8573f10ab61436d1f5e42c
Brahim Jandri <125567781+BrahimJandri@users.noreply.github.com>
2023-09-06 16:04:09 +0100

    Add files via upload

---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjandri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 13:27:15 by bjandri           #+#    #+#             */
+/*   Updated: 2023/09/06 13:34:40 by bjandri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 32)
+		{
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[str[i] / 16]);
+			ft_putchar("0123456789abcdef"[str[i] % 16]);
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+}
